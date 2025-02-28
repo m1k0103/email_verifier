4ce8bd1994f0e7a0f35ef67f49472d6a91837a8a
miki <84585119+miko1034@users.noreply.github.com>
2025-02-28 12:30:57 +0000

    changes

--- email_verifier/__init__.py
+++ email_verifier/__init__.py
@@ -1,2 +1,15 @@
+import os
+
 def start():
-	
+	# create input file folder
+	if "inputs" not in os.listdir("./email_verifier/"):
+		os.mkdir("email_verifier/inputs")
+
+	# creates the paths.cfg file
+	if "paths.cfg" not in os.listdir():
+		f = open("paths.cfg", "w+")
+		f.write("""paths: ['/your/path/here', './second/path/here']""")
+		f.close()
+
+	from email_verifier.main import main
+	main()
